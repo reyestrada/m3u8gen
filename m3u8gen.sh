88ffa849dcd2ff200f63a9318cd178e1ea750841
Rey Estrada <reyarmando@gmail.com>
2020-09-21 15:45:53 +0000

    improving the playlist generation

--- m3u8gen.sh
+++ m3u8gen.sh
@@ -7,9 +7,9 @@
 # Execute against the main directory
 # m3u8gen.sh Music  // where Music is the directory of music
 # if not directory provided, script will be executed in current path
-# This will read all folders 
+# This will read all folders and subfolders (maxdepth = 3) 
 # and will generate a playlist per folder
-# Playlists will be placed inside every folder
+# Playlists will be placed in the current directory
 # Playlist names will be the same as playlist folder
 # Recommendation: keep names and folders short
 
@@ -25,13 +25,15 @@ echo
 # find . -maxdepth 1 -type f -printf "%f\n"
 # find . -maxdepth 1 -type d -printf "%f\n"
 
-playlists=$(find . -maxdepth 1 -type d -printf "%f\n" | grep -v Playlists | grep -v ^\.$)
+#playlists=$(find . -maxdepth 1 -type d -printf "%f\n" | grep -v Playlists | grep -v ^\.$)
+directories=$(find . -maxdepth 3 -type d | sed 's/\.\///g')
 
-while read playlist; do 
+while read directory; do 
+	playlist=$(find "$directory" -maxdepth 1 -type d -printf "%f" | tr " " "_"
 	echo "Generating $playlist...";
-	list=$(find "$playlist" -maxdepth 1 -type f -iname "*.mp3" -printf "%f\n")
-	echo "$list" > "${playlist}/${playlist}.m3u8"
-	echo "${playlist}/$playlist.m3u8 generated!"
+	list=$(find "$directory" -maxdepth 1 -type f -iname "*.mp3")
+	echo "$list" > "${playlist}.m3u8"
+	echo "${playlist}.m3u8 generated!"
 	echo
-done <<< "$playlists"
+done <<< "$directories"
 
